fbe6e67b8c58bf00e1be06f889e5c3dd0dff79d1
Tom Pritchard <tompritchard1996@gmail.com>
2017-09-01 04:31:29 +0100

    Implemented saving tag_types to module file, for use in API Querying

--- getMasterList.js
+++ getMasterList.js
@@ -1,4 +1,5 @@
 var connection = require(`./interface`);
+var fs = require('fs');
 
 //This is a self calling function. This exports as normal, but allows for the module.parent check.
 //If there is no module parent, we've ran this file standalone, so call the getMasterList() method.
@@ -6,7 +7,29 @@ var connection = require(`./interface`);
 (function(){
     exports.getMasterList = function() {
       connection.request(`/getMasterList`, `GET`, (response) => {
-        console.log(response);
+        var content = "module.exports = {\n"
+
+        response.forEach((tag_type, index, array) => {
+          content +=
+            `\t${tag_type.name}: {\n`
+          + `\t\ttag_table_type: '${tag_type.tag_table_type}',\n`
+          + `\t\tcreation_date: '${tag_type.creation_date}'\n`
+          + `\t}`;
+
+          if (index !== array.length-1) {
+            content += `,\n`
+          }
+        });
+
+        content += `\n}\n`;
+
+        fs.writeFile("./tag_types.js", content, function(err) {
+          if(err) {
+              return console.log(err);
+          }
+
+          console.log("File saved!");
+        });
       });
     };
 
